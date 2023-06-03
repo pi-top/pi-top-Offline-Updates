3c77b4ea42ed3c68f54ad49f5cde51fa83d5a694
Jorge Capona <jcapona@gmail.com>
2023-06-03 17:00:50 -0400

    Handle already downloaded packages

--- download-packages.sh
+++ download-packages.sh
@@ -17,12 +17,28 @@ install_dependencies() {
 generate_packages_list() {
     # List all installed packages
     dpkg-query -W -f='${Status} ${package}=${version}\n' | grep "^install ok installed" | awk '{print $4}' >"${PACKAGES_FOLDER}/${PACKAGES_FILE}"
-    cat "${PACKAGES_FILE}"
+    cat "${PACKAGES_FOLDER}/${PACKAGES_FILE}"
 }
 
 download_packages() {
     cd "${PACKAGES_FOLDER}"
     while IFS= read -r package; do
+        package_name=$(echo "${package}" | cut -d '=' -f 1)
+        package_version=$(echo "${package}" | cut -d '=' -f 2)
+
+        # Handle already downloaded packages
+        downloaded_file=$(find . -name "${package_name}_*.deb")
+        if [ -f "${downloaded_file}" ]; then
+            downloaded_file_has_correct_version=$(echo "${downloaded_file}" | grep "${package_version}" || true)
+            if [ -n "${downloaded_file_has_correct_version}" ]; then
+                echo "Package ${package_name} already downloaded with version ${package_version}: ${downloaded_file}. Skipping..."
+                continue
+            else
+                echo "Older version for package ${package_name} found: ${downloaded_file}. Removing..."
+                rm -f "${downloaded_file}"
+            fi
+        fi
+
         echo "Downloading ${package}..."
         DOWNLOAD_CMD="apt download ${package} &>/dev/null"
         CMD_OUTPUT=$(eval "${DOWNLOAD_CMD}")
@@ -31,7 +47,7 @@ download_packages() {
             echo "Error: ${CMD_OUTPUT}"
             echo "Failed to download packlage ${package}. Retrying..."
             sleep 1
-            eval "${DOWNLOAD_CMD} || true"
+            eval "${DOWNLOAD_CMD} || true" # TODO: remove '|| true'
         done
     done <"${PACKAGES_FILE}"
     cd "${CURR_FOLDER}"
