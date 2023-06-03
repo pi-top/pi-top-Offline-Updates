d1991aec1ec96ff9b0d6802984d0663607cb7a33
Jorge Capona <jcapona@gmail.com>
2023-06-02 20:43:59 -0400

    Cache debs

--- download-packages.sh
+++ download-packages.sh
@@ -3,6 +3,7 @@
 PACKAGES_FOLDER="packages"
 PACKAGES_FILE="packages.list"
 COMPRESSED_FILE="packages.tar.gz"
+CURR_FOLDER="$(pwd)"
 
 install_dependencies() {
     apt update
@@ -15,38 +16,39 @@ install_dependencies() {
 
 generate_packages_list() {
     # List all installed packages
-    dpkg-query -W -f='${Status} ${package}=${version}\n' | grep "^install ok installed" | awk '{print $4}' >"${PACKAGES_FILE}"
+    dpkg-query -W -f='${Status} ${package}=${version}\n' | grep "^install ok installed" | awk '{print $4}' >"${PACKAGES_FOLDER}/${PACKAGES_FILE}"
+    cat "${PACKAGES_FILE}"
 }
 
 download_packages() {
     cd "${PACKAGES_FOLDER}"
     while IFS= read -r package; do
-        DOWNLOAD_CMD="apt download ${package}"
-        eval "${DOWNLOAD_CMD}"
-
+        echo "Downloading ${package}..."
+        DOWNLOAD_CMD="apt download ${package} &>/dev/null"
+        CMD_OUTPUT=$(eval "${DOWNLOAD_CMD}")
         # Retry on failure
         while [ $? -ne 0 ]; do
+            echo "Error: ${CMD_OUTPUT}"
             echo "Failed to download packlage ${package}. Retrying..."
             sleep 1
-            eval "${DOWNLOAD_CMD}"
+            eval "${DOWNLOAD_CMD} || true"
         done
-
     done <"${PACKAGES_FILE}"
-
+    cd "${CURR_FOLDER}"
 }
 
 generate_packages_apt_file() {
+    cd "${CURR_FOLDER}"
     dpkg-scanpackages "${PACKAGES_FOLDER}" /dev/null | gzip -9c >"${PACKAGES_FOLDER}/Packages.gz"
 }
 
 compress_folder() {
+    cd "${CURR_FOLDER}"
     tar -czvf "${COMPRESSED_FILE}" "${PACKAGES_FOLDER}"
 }
 
 main() {
     mkdir -p "${PACKAGES_FOLDER}"
-    cd "${PACKAGES_FOLDER}"
-
     install_dependencies
     generate_packages_list
     download_packages
