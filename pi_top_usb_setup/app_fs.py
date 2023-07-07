106b9e9f8e46b82cdc2b789a16f99dc4f64bcec4
Jorge Capona <jcapona@users.noreply.github.com>
2023-07-07 10:00:26 -0400

    Configure pi-top & complete onboarding (#6)

--- pi_top_usb_setup/app_fs.py
+++ pi_top_usb_setup/app_fs.py
@@ -1,8 +1,26 @@
+import json
 import logging
 from pathlib import Path
 from typing import Callable, Optional
 
 from pitop.common.command_runner import run_command
+from pt_os_web_portal.backend.helpers.finalise import (
+    configure_landing,
+    deprioritise_openbox_session,
+    disable_ap_mode,
+    enable_firmware_updater_service,
+    enable_further_link_service,
+    enable_pt_miniscreen,
+    onboarding_completed,
+    restore_files,
+    stop_onboarding_autostart,
+    update_eeprom,
+)
+from pt_os_web_portal.backend.helpers.keyboard import set_keyboard_layout
+from pt_os_web_portal.backend.helpers.language import set_locale
+from pt_os_web_portal.backend.helpers.registration import set_registration_email
+from pt_os_web_portal.backend.helpers.timezone import set_timezone
+from pt_os_web_portal.backend.helpers.wifi_country import set_wifi_country
 
 from pi_top_usb_setup.exceptions import ExtractionError, NotEnoughSpaceException
 from pi_top_usb_setup.utils import (
@@ -21,6 +39,7 @@ class AppFilesystem:
         self.USB_SETUP_FILE = f"{mount_point}/pi-top-usb-setup.tar.gz"
         self.TEMP_FOLDER = "/tmp"
         self.SETUP_FOLDER = f"{self.TEMP_FOLDER}/pi-top-usb-setup"
+        self.DEVICE_CONFIG = f"{self.SETUP_FOLDER}/pi-top_config.json"
         self.UPDATES_FOLDER = f"{self.SETUP_FOLDER}/updates"
 
         if not any(
@@ -33,6 +52,7 @@ class AppFilesystem:
                 f"Files {self.USB_SETUP_FILE} or {self.UPDATES_FOLDER} not found, exiting ..."
             )
 
+        self.requires_reboot = False
         self.device = None
         if mount_point:
             self.device = run_command(
@@ -70,3 +90,67 @@ class AppFilesystem:
             logger.info(f"File {self.USB_SETUP_FILE} extracted into {self.TEMP_FOLDER}")
         except Exception as e:
             raise ExtractionError(f"Error extracting '{self.USB_SETUP_FILE}': {e}")
+
+    def configure_device(self, on_progress: Optional[Callable] = None) -> None:
+        if not Path(self.DEVICE_CONFIG).exists():
+            logger.info("No device configuration file found; skipping....")
+            return
+
+        # setting the keyboard layout requires a layout and a variant
+        lookup = {
+            "language": set_locale,
+            "country": set_wifi_country,
+            "time_zone": set_timezone,
+            "keyboard_layout": lambda layout_and_variant_arr: set_keyboard_layout(
+                *layout_and_variant_arr
+            ),
+            "registration_email": set_registration_email,
+        }
+
+        logger.info(f"Configuring device using {self.DEVICE_CONFIG}")
+        with open(self.DEVICE_CONFIG) as file:
+            content = file.read()
+            config = json.loads(content)
+
+        for i, (key, function) in enumerate(lookup.items()):
+            if key not in config:
+                logger.info(f"'{key}' not found in configuration file, skipping...")
+                continue
+
+            try:
+                logger.info(f"{key}: Executing {function} with '{config.get(key)}'")
+                function(config.get(key))
+                if callable(on_progress):
+                    on_progress(float(100.0 * i / len(lookup)))
+            except Exception as e:
+                logger.error(f"{e}")
+
+    def complete_onboarding(self, on_progress: Optional[Callable] = None) -> None:
+        if onboarding_completed():
+            logger.info("Device already onboarded")
+            return
+
+        functions = (
+            enable_firmware_updater_service,
+            enable_further_link_service,
+            deprioritise_openbox_session,
+            restore_files,
+            configure_landing,
+            stop_onboarding_autostart,
+            update_eeprom,
+            enable_pt_miniscreen,
+            disable_ap_mode,
+        )
+
+        logger.info("Completing onboarding for device ...")
+        for i, func in enumerate(functions):
+            try:
+                logger.info(f"Executing {func} ...")
+                func()
+            except Exception as e:
+                logger.error(f"{e}")
+
+            if callable(on_progress):
+                on_progress(float(100.0 * i / len(functions)))
+
+        self.requires_reboot = True
--- pi_top_usb_setup/pages/setup_status.py
+++ pi_top_usb_setup/pages/setup_status.py
@@ -1,5 +1,7 @@
 import logging
+from time import sleep
 
+from pitop.common.command_runner import run_command
 from pt_miniscreen.components.mixins import HasGutterIcons
 from pt_miniscreen.core.component import Component
 from pt_miniscreen.core.components import Text
@@ -12,11 +14,14 @@ logger = logging.getLogger(__name__)
 
 
 class SetupStatusPage(Component, HandlesAllButtons, HasGutterIcons):
-    def __init__(self, message: str, **kwargs):
+    def __init__(self, message: str, requires_reboot: bool, **kwargs):
         super().__init__(**kwargs)
+        self.requires_reboot = requires_reboot
+        self._text = message
         self.text_component = self.create_child(
             Text,
-            text=message,
+            text=self._text,
+            get_text=lambda: self._text,
             font_size=10,
             align="center",
             vertical_align="center",
@@ -24,7 +29,14 @@ class SetupStatusPage(Component, HandlesAllButtons, HasGutterIcons):
         )
 
     def handle_button_press(self):
-        close_app()
+        if self.requires_reboot:
+            # Display a 'please wait' message in case the reboot animation
+            # doesn't get displayed
+            self._text = "Please wait ..."
+            sleep(2)
+            run_command("reboot", 20)
+        else:
+            close_app()
 
     def render(self, image):
         return self.text_component.render(image)
