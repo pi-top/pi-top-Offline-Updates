53c937bef9f5088c5dd77b7866039df5d51b6518
Jorge Capona <jcapona@users.noreply.github.com>
2024-08-28 09:19:08 -0400

    Generate update file for bookworm (#17)

--- download-packages.sh
+++ download-packages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PACKAGES_FOLDER="updates"
-COMPRESSED_FILE="updates.tar.gz"
+PACKAGES_FOLDER="${1:-updates}"
+COMPRESSED_FILE="${2:-updates.tar.gz}"
 CURR_FOLDER="$(pwd)"
 PACKAGES_FILE="${CURR_FOLDER}/packages.list"
+ERROR_LOG="${CURR_FOLDER}/download_error.list"
 
 install_dependencies() {
     apt update
@@ -20,43 +21,64 @@ generate_packages_list() {
     cat "${PACKAGES_FILE}"
 }
 
+download_package() {
+    package="${1}"
+
+    package_name=$(echo "${package}" | cut -d '=' -f 1)
+    package_version=$(echo "${package}" | cut -d '=' -f 2)
+
+    # Handle already downloaded packages
+    downloaded_file=$(find . -name "${package_name}_*.deb")
+    if [ -f "${downloaded_file}" ]; then
+        # TODO: this verification won't work with files that have encoded some characters in the name
+        # eg: version is '1:5.8.1+dfsg-2' but filename has '1%3a5.8.1+dfsg-2'
+        downloaded_file_has_correct_version=$(echo "${downloaded_file}" | grep "${package_version}" || true)
+        if [ -n "${downloaded_file_has_correct_version}" ]; then
+            echo "Package ${package_name} already downloaded with version ${package_version}: ${downloaded_file}. Skipping..."
+            continue
+        else
+            echo "Older version for package ${package_name} found: ${downloaded_file}. Removing..."
+            rm -f "${downloaded_file}"
+        fi
+    fi
+
+    echo "Downloading ${package}..."
+    DOWNLOAD_CMD="apt download ${package} &>/dev/null"
+    CMD_OUTPUT=$(eval "${DOWNLOAD_CMD}")
+    # Retry on failure
+    if [ $? -ne 0 ]; then
+        echo "Failed to download package ${package}. Retrying with latest version..."
+        sleep 1
+
+        DOWNLOAD_CMD="apt download ${package_name} &>/dev/null"
+        # Retry a few times before giving up
+        for i in {1..10}; do
+            CMD_OUTPUT=$(eval "${DOWNLOAD_CMD}")
+            echo "Downloading ${package_name}, retry ${i}..."
+            if [ $? -eq 0 ]; then
+                echo "Downloading ${package_name}, retry ${i}: success!"
+                break
+            fi
+
+            # On failure, log package name
+            if [ $i -eq 10 ]; then
+                echo "Failed to download package ${package_name}. Skipping..."
+                echo "${package}" >>"${ERROR_LOG}"
+            fi
+
+            sleep 1
+        done
+    fi
+}
+
 download_packages() {
     cd "${PACKAGES_FOLDER}"
     while IFS= read -r package; do
-        package_name=$(echo "${package}" | cut -d '=' -f 1)
-        package_version=$(echo "${package}" | cut -d '=' -f 2)
-
-        # Handle already downloaded packages
-        downloaded_file=$(find . -name "${package_name}_*.deb")
-        if [ -f "${downloaded_file}" ]; then
-            # TODO: this verification won't work with files that have encoded some characters in the name
-            # eg: version is '1:5.8.1+dfsg-2' but filename has '1%3a5.8.1+dfsg-2'
-            downloaded_file_has_correct_version=$(echo "${downloaded_file}" | grep "${package_version}" || true)
-            if [ -n "${downloaded_file_has_correct_version}" ]; then
-                echo "Package ${package_name} already downloaded with version ${package_version}: ${downloaded_file}. Skipping..."
-                continue
-            else
-                echo "Older version for package ${package_name} found: ${downloaded_file}. Removing..."
-                rm -f "${downloaded_file}"
-            fi
-        fi
-
-        echo "Downloading ${package}..."
-        DOWNLOAD_CMD="apt download ${package} &>/dev/null"
-        CMD_OUTPUT=$(eval "${DOWNLOAD_CMD}")
-        # Retry on failure
-        if [ $? -ne 0 ]; then
-            echo "Failed to download package ${package}. Retrying with latest version..."
-            sleep 1
-
-            echo "Downloading ${package_name}..."
-            DOWNLOAD_CMD="apt download ${package_name} &>/dev/null"
-            CMD_OUTPUT=$(eval "${DOWNLOAD_CMD}")
-            if [ $? -ne 0 ]; then
-                echo "Failed to download package ${package_name}. Skipping..."
-            fi
-        fi
-
+        download_package "${package}" &
+        # Limit number of parallel downloads to 4
+        while [ $(jobs | wc -l) -ge 4 ]; do
+            sleep 0.5
+        done
     done <"${PACKAGES_FILE}"
     cd "${CURR_FOLDER}"
 }
@@ -85,6 +107,15 @@ compress_folder() {
     tar -czvf "${COMPRESSED_FILE}" "${PACKAGES_FOLDER}"
 }
 
+print_error_log() {
+    if [ -f "${ERROR_LOG}" ]; then
+        echo "Failed to download the following packages:"
+        cat "${ERROR_LOG}"
+    else
+        echo "All packages downloaded successfully!"
+    fi
+}
+
 main() {
     mkdir -p "${PACKAGES_FOLDER}"
     install_dependencies
@@ -92,6 +123,7 @@ main() {
     download_packages
     generate_packages_apt_file
     compress_folder
+    print_error_log
 }
 
 main
